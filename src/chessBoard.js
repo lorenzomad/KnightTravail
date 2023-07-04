9cce0f4c693a0699fd1d258d17864abd05ab602f
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-07-04 15:14:03 +0000

    feat generate links and tests

--- src/chessBoard.js
+++ src/chessBoard.js
@@ -1,11 +1,9 @@
 const ChessBoard = () => {
-    
-
     /* Method to create the board, returns the board array with the
     * names of each cell [x,y]
     * returns output, the array containing the board
     */
-    function generateBoard() {
+    const generateBoard = () => {
         let output = [];
         let line = [];
 
@@ -20,8 +18,50 @@ const ChessBoard = () => {
     }
     let board = generateBoard()
 
+    /* creates the links from each cell to all the cells which are possibly linked with it*/
+    const generateLinks = () => {
+        //list all possible moves
+        const moves = [
+            [1,2],
+            [1,-2],
+            [2,1],
+            [2,-1],
+            [-1, 2],
+            [-1, -2],
+            [-2, 1],
+            [-2, -1]
+        ]
+
+        let links = []
+        let linksLine = []
+        let linksCell = []
+        let newCell
+
+        board.forEach(line => {
+            line.forEach(cell => {
+                moves.forEach(move => {
+                    newCell = cell.map((num, idx) => {
+                        return num + move[idx];
+                    })
+                    if (newCell[0] < 8 && newCell[0] >= 0 && newCell[1] < 8 && newCell[1] >= 0) {
+                        linksCell.push(newCell)
+                    }
+                });
+                linksLine.push(linksCell)
+                linksCell = []
+            });
+            links.push(linksLine)
+            linksLine = []
+            
+        });
+        return links;
+    }
+
+    let links = generateLinks()
+
     return {
-        board
+        board,
+        links
     }
 }
 
